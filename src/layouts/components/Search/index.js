ea0939d625f8d47e71abd0ac6f190128d49ff822
MinhVuongTran <89984852+MinhVuongTran@users.noreply.github.com>
2022-07-25 23:08:33 +0700

    Tái cấu trúc và tối ưu code #1

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -5,7 +5,7 @@ import classNames from 'classnames/bind';
 import { useEffect, useState, useRef } from 'react';
 import 'tippy.js/dist/tippy.css';
 
-import * as searchServices from '~/apiServices/searchServices';
+import * as searchServices from '~/services/searchService';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
