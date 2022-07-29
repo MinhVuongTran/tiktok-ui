3f922c57fdd60cca7947f67eab634e60d9c15c71
MinhVuongTran <89984852+MinhVuongTran@users.noreply.github.com>
2022-07-29 09:58:48 +0700

    Áp dụng PropTypes vào dự án

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types';
 import './GlobalStyle.scss';
 
 const index = ({ children }) => {
     return children;
 };
+
+index.propTypes = {
+    children: PropTypes.node.isRequired
+}
 
 export default index;
