a288239c360901d6531e653190d3eb4c15bcc2a3
MinhVuongTran <89984852+MinhVuongTran@users.noreply.github.com>
2022-07-30 21:47:59 +0700

    Giải quyết vấn đề nhiều file index.js

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -1,12 +1,1 @@
-import PropTypes from 'prop-types';
-import './GlobalStyle.scss';
-
-const index = ({ children }) => {
-    return children;
-};
-
-index.propTypes = {
-    children: PropTypes.node.isRequired
-}
-
-export default index;
+export { default } from './GlobalStyle';
